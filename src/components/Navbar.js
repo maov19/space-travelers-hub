229a23bb38c009907876340fbea20039162427b4
mossa <martin.ossa.8@gmail.com>
2023-04-10 12:33:03 +0200

    Create empty navigation

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import icon from '../components/images/planet.png'
+
+const Navigation = () => {
+    return (
+        <nav>
+            <img src= {icon} />
+        </nav>
+    )
+}
+
+export { Navigation };
