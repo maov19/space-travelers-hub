05bbe03854343e5ffc507c574d4b40a41f40de8c
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-04-11 09:35:19 +0300

    Setup the NavLink

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import { Navigation } from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-    </div>
+    <>
+    <NavLink />
+    </>
   );
 }
 
---
+++ src/components/NavLink.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavLink = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Rockets</li>
+        <li>Missions</li>
+        <li>|</li>
+        <li>MyProfile</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavLink
