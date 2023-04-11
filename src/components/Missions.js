da6219c4547b56a590f92bfe4802582d57685af6
mossa <martin.ossa.8@gmail.com>
2023-04-11 18:05:13 +0200

    Add missions to display

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,26 +1,30 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchMissions } from '../redux/missions/missionsSlice';
-// import '../styles/missionsStyles.css';
+import { useSelector } from 'react-redux';
+import './styles/missionsStyles.css';
 
 const Missions = () => {
-  const dispatch = useDispatch();
-  const missions = useSelector((state) => state.missions);
+  const missions = useSelector((state) => state.missions.missions);
 
   return (
-    <div className="missions-grid">
-      <div className="missions-header">Mission Name</div>
-      <div className="missions-header">Mission Description</div>
-      <div className="missions-header">Status</div>
-      {missions.map((mission) => (
-        <div key={mission.mission_id}>
-          <div className="missions-name">{mission.mission_name}</div>
-          <div className="missions-description">{mission.description}</div>
-          <div className="missions-buttons">
-            Status
-          </div>
+    <div>
+      <div className="missions-grid">
+        <div className="header-row">
+          <div className="missions-header">Mission</div>
+          <div className="missions-header-description">Description</div>
+          <div className="missions-header-status">Status</div>
         </div>
-      ))}
+        {missions.map((mission) => (
+          <div key={mission.mission_id} className="mission-row">
+            <div className="mission-name">{mission.mission_name}</div>
+            <div className="mission-description">{mission.description}</div>
+            <div className="mission-status">
+              <button className="member-button" type="button">NOT A MEMBER</button>
+            </div>
+            <div className="mission-status">
+              <button className="mission-button" type="button">Join Mission</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+
 import './styles/nav.css';
+import Missions from './Missions';
 
 const View = () => (
   <nav id="nav-bar">
     <NavLink to="/">Rockets</NavLink>
-    <NavLink to="/missions">Missions</NavLink>
+    <NavLink to="/missions" element={<Missions />}>Missions</NavLink>
     <NavLink to="/profile">MyProfile</NavLink>
   </nav>
 );
