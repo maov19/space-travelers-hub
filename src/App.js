7930a1405d82910c9b9a4ed4a3b3a6699ac0b492
mossa <martin.ossa.8@gmail.com>
2023-04-11 11:38:53 +0200

    Add missions to main display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import View from './components/View';
+import Missions from './components/Missions';
 
 function App() {
   return (
     <>
       <View />
+      <Missions />
     </>
   );
 }
