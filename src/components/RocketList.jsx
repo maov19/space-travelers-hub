97685a0ae743f1abc79223b16e35f0bf23d4cfbf
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-04-11 21:18:06 +0300

    Style Rockets buttons

--- src/components/RocketList.jsx
+++ src/components/RocketList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { reserveRocket } from '../redux/rockets/rocketsSlice';
+import { btnBookStyle, reservedStyle, btnCancelStyle } from './RocketBtnStyles';
 
 const RocketList = ({ item }) => {
   const dispatch = useDispatch();
@@ -9,31 +10,6 @@ const RocketList = ({ item }) => {
     id, rocket_name: rocketName, description, flickr_images: rocketImage,
   } = item;
   const [firstImage] = rocketImage;
-
-  const btnBookStyle = {
-    backgroundColor: '#007bff',
-    border: 0,
-    borderRadius: 4,
-    color: '#fff',
-    padding: 10,
-  };
-
-  const reservedStyle = {
-    color: '#fff',
-    padding: 2,
-    backgroundColor: '#18a2b8',
-    borderRadius: 5,
-    fontSize: 10,
-    marginRight: 5,
-  };
-
-  const btnCancelStyle = {
-    backgroundColor: '#fff',
-    border: '1px solid #000',
-    borderRadius: 4,
-    color: '#989ea3',
-    padding: 10,
-  };
 
   const handleClick = (id) => {
     dispatch(reserveRocket(id));
