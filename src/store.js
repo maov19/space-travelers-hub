9a63d0954de38d7405f9eb34302991f99f9d8aa5
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-04-11 11:14:58 +0300

    Add store

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    mission: missionsReducer,
+    rocket: rocketsReducer,
+  },
+});
+
+export default store
