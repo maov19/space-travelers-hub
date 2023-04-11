1c8c682d7f4c25e966570889269bec574a18050e
mossa <martin.ossa.8@gmail.com>
2023-04-11 13:56:39 +0200

    Fix styling issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import View from './components/View';
-import Missions from './components/Missions';
 
 function App() {
   return (
     <>
       <View />
-      <Missions />
     </>
   );
 }
