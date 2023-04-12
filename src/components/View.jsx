5d038fe52fa57d9ddcb8a2bf97cfce38e2512a32
mossa <martin.ossa.8@gmail.com>
2023-04-12 15:38:29 +0200

    Fix styling issues

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -9,10 +9,12 @@ const View = () => (
   <nav id="nav-bar">
     <img src={logo} alt="logo" />
     <h1>Space Travelers&apos; Hub</h1>
-    <NavLink to="/">Rockets</NavLink>
-    <NavLink to="/missions" element={<Missions />}>Missions</NavLink>
-    <p>|</p>
-    <NavLink to="/profile">My Profile</NavLink>
+    <div className="nav-links">
+      <NavLink to="/">Rockets</NavLink>
+      <NavLink to="/missions" element={<Missions />}>Missions</NavLink>
+      <span>|</span>
+      <NavLink to="/profile">My Profile</NavLink>
+    </div>
   </nav>
 );
 
