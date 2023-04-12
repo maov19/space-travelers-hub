b644ff4aef1f9136a9380b75c5d7745f9b378972
mossa <martin.ossa.8@gmail.com>
2023-04-12 15:11:14 +0200

    Fix navbar styles

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import logo from '../images/planet.png';
 
 import './styles/nav.css';
 import Missions from './Missions';
 
 const View = () => (
   <nav id="nav-bar">
+    <img src={logo} alt="logo" />
+    <h1>Space Travelers&apos; Hub</h1>
     <NavLink to="/">Rockets</NavLink>
     <NavLink to="/missions" element={<Missions />}>Missions</NavLink>
-    <NavLink to="/profile">MyProfile</NavLink>
+    <p>|</p>
+    <NavLink to="/profile">My Profile</NavLink>
   </nav>
 );
 
