3e8668474e212caf973950edcadedae38b318e96
mossa <martin.ossa.8@gmail.com>
2023-04-12 16:32:13 +0200

    Add table styling to missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -15,41 +15,56 @@ const Missions = () => {
 
   return (
     <div>
-      <div className="missions-grid">
-        <div className="header-row">
-          <div className="missions-header">Mission</div>
-          <div className="missions-header-description">Description</div>
-          <div className="missions-header-status">Status</div>
-        </div>
-        {missions.map((mission) => (
-          <div key={mission.mission_id} className="mission-row">
-            <div className="mission-name">{mission.mission_name}</div>
-            <div className="mission-description">{mission.description}</div>
-            <div className="mission-status">
-              {mission.reserved ? (
-                <button className="member-button" type="button" style={missionButtonStyles.activeMember}>
-                  Active Member
-                </button>
-              ) : (
-                <button className="member-button" type="button" style={missionButtonStyles.notMember}>
-                  NOT A MEMBER
-                </button>
-              )}
-            </div>
-            <div className="mission-status">
-              {mission.reserved ? (
-                <button className="mission-button" type="button" style={missionButtonStyles.leaveMission} onClick={() => handleCancelMission(mission.mission_id)}>
-                  Leave Mission
-                </button>
-              ) : (
-                <button className="mission-button" type="button" style={missionButtonStyles.joinMission} onClick={() => handleJoinMission(mission.mission_id)}>
-                  Join Mission
-                </button>
-              )}
-            </div>
-          </div>
-        ))}
-      </div>
+      <table className="missions-table">
+        <thead>
+          <tr className="header-row">
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <tr key={mission.mission_id} className="mission-row">
+              <td className="mission-name">{mission.mission_name}</td>
+              <td className="mission-description">{mission.description}</td>
+              <td className="mission-status">
+                {mission.reserved ? (
+                  <button className="member-button" type="button" style={missionButtonStyles.activeMember}>
+                    Active Member
+                  </button>
+                ) : (
+                  <button className="member-button" type="button" style={missionButtonStyles.notMember}>
+                    NOT A MEMBER
+                  </button>
+                )}
+              </td>
+              <td className="mission-status">
+                {mission.reserved ? (
+                  <button
+                    className="mission-button"
+                    type="button"
+                    style={missionButtonStyles.leaveMission}
+                    onClick={() => handleCancelMission(mission.mission_id)}
+                  >
+                    Leave Mission
+                  </button>
+                ) : (
+                  <button
+                    className="mission-button"
+                    type="button"
+                    style={missionButtonStyles.joinMission}
+                    onClick={() => handleJoinMission(mission.mission_id)}
+                  >
+                    Join Mission
+                  </button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
